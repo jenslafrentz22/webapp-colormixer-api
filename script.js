118c95f1dab81180deb85418128b40419729b84d
Jens Lafrentz <j.lafrentz@web.de>
2023-01-27 10:00:55 +0100

    feat: it works!

---
+++ script.js
@@ -0,0 +1,52 @@
+const red = document.querySelector("#red");
+const green = document.querySelector("#green");
+const blue = document.querySelector("#blue");
+const hex = document.querySelector("#hex");
+const inputs = document.querySelectorAll("input");
+const button = document.querySelector("button");
+let count;
+
+for (i = 0; i < inputs.length; i++) {
+  inputs[i].step = 1;
+  inputs[i].min = 0;
+  inputs[i].max = 255;
+}
+
+red.value = 255;
+green.value = 105;
+blue.value = 180;
+
+function changeBGColor(rcolor, redvalue, greenvalue, bluevalue) {
+  // value = string, daher +
+  const redbg = (+red.value).toString(16);
+  const greenbg = (+green.value).toString(16);
+  const bluebg = (+blue.value).toString(16);
+  let bgcolor = `#${redbg}${greenbg}${bluebg}`;
+  console.log(count);
+  if (count === 1) {
+    bgcolor = rcolor;
+    red.value = redvalue;
+    green.value = greenvalue;
+    blue.value = bluevalue;
+  }
+  count = 0;
+  document.body.style.backgroundColor = bgcolor;
+  hex.innerText = bgcolor;
+}
+changeBGColor();
+document.addEventListener("input", changeBGColor);
+
+// Random Color Button
+function loadRandomColor() {
+  fetch("https://dummy-apis.netlify.app/api/color")
+    .then((response) => response.json())
+    .then((data) => {
+      redvalue = data.rgb.r;
+      greenvalue = data.rgb.g;
+      bluevalue = data.rgb.b;
+      // Werte setzen über vorhandene changeBGColor()
+      changeBGColor(data.color, redvalue, greenvalue, bluevalue);
+    });
+  count = 1;
+}
+button.addEventListener("click", loadRandomColor);
