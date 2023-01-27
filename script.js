71c2a08cc938b12f7b4051944ea01bd506741d00
Jens Lafrentz <j.lafrentz@web.de>
2023-01-27 10:30:18 +0100

    fix: count is now isButtonClicked

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const blue = document.querySelector("#blue");
 const hex = document.querySelector("#hex");
 const inputs = document.querySelectorAll("input");
 const button = document.querySelector("button");
-let count;
+let isButtonClicked;
 
 for (i = 0; i < inputs.length; i++) {
   inputs[i].step = 1;
@@ -22,14 +22,14 @@ function changeBGColor(rcolor, redvalue, greenvalue, bluevalue) {
   const greenbg = (+green.value).toString(16);
   const bluebg = (+blue.value).toString(16);
   let bgcolor = `#${redbg}${greenbg}${bluebg}`;
-  console.log(count);
-  if (count === 1) {
+  console.log(isButtonClicked);
+  if (isButtonClicked === 1) {
     bgcolor = rcolor;
     red.value = redvalue;
     green.value = greenvalue;
     blue.value = bluevalue;
   }
-  count = 0;
+  isButtonClicked = 0;
   document.body.style.backgroundColor = bgcolor;
   hex.innerText = bgcolor;
 }
@@ -47,6 +47,6 @@ function loadRandomColor() {
       // Werte setzen über vorhandene changeBGColor()
       changeBGColor(data.color, redvalue, greenvalue, bluevalue);
     });
-  count = 1;
+  isButtonClicked = 1;
 }
 button.addEventListener("click", loadRandomColor);
